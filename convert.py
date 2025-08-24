d461b5bd8805cc878c85a0b6217109f23cc46cb5
SimoHellgren <hellgrensimo@gmail.com>
2025-08-24 20:22:20 +0300

    prepare for timestamps

--- convert.py
+++ convert.py
@@ -1,6 +1,8 @@
 import json
+from datetime import datetime
 from pathlib import Path
 
+import pytz
 import yaml
 
 from build import parse_recipe2
@@ -8,11 +10,19 @@ from build import parse_recipe2
 result = []
 
 for file in Path("recipes").glob("**/*.yml"):
+    created_ts = file.stat().st_ctime
+    created = datetime.fromtimestamp(created_ts, pytz.utc).strftime("%Y-%m-%d %H:%M:%S")
+
+    updated_ts = file.stat().st_mtime
+    updated = datetime.fromtimestamp(updated_ts, pytz.utc).strftime("%Y-%m-%d %H:%M:%S")
+
     with open(file, encoding="utf-8") as f:
         data = yaml.safe_load(f)
 
     recipe = parse_recipe2(data)
-    result.append({**recipe, "id": file.stem})
+    result.append(
+        {**recipe, "id": file.stem, "created_at": created, "updated_at": updated}
+    )
 
 with open("recipes.json", "w", encoding="utf-8") as f:
     json.dump(result, f, indent=2)
